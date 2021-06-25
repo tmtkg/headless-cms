3803161940f57cf3660051f70e17686b0cd3c1e5
tmtkg <tom.takagi.c@gmail.com>
2021-06-26 00:04:04 +0900

    fix: APIのURLに/が不足していたので追加

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -13,7 +13,7 @@ export default {
   // asyncDataの引数のparamsの中にslugというプロパティが含まれており、こちらにURLからのパスが格納されてきます。
   async asyncData({ params }) {
     const { data } = await axios.get(
-      `https:/tmtkg.microcms.io/api/v1/blog/${params.slug}`,
+      `https://tmtkg.microcms.io/api/v1/blog/${params.slug}`,
       {
         headers: { 'X-API-KEY': '90a9f7ef-a60e-49e0-bbbc-c9a154625917' }
       }
